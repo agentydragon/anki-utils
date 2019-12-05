0028bd0e9a63a2e76f4b9397fe11fe20f43afcc3
Michal Pokorný (Rai) <agentydragon@gmail.com>
2019-12-05 22:17:49 +0100

    Add cloze

---
+++ src/cloze/cloze.js
@@ -0,0 +1,12 @@
+(function() {
+const header = document.querySelector("h1");
+if (header.textContent.length == 0) {
+  const deck = document.querySelector("#agentydragon-deck").textContent;
+  const deckHierarchy = deck.split("::");
+  const innerDeck = deckHierarchy[deckHierarchy.length - 1];
+  // "05 German" --> "German"
+  // "*05 German" --> "German"
+  const sanitizedDeck = innerDeck.replace(/^.?\d*\\ ?(.*)$/, "$1");
+  header.textContent = sanitizedDeck;
+}
+})();
