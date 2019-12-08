f433fac27365259d391a58c1bdf5775d294f8470
Michal Pokorný (Rai) <agentydragon@gmail.com>
2019-12-08 16:57:14 +0100

    Add keyboard shortcut to slug

--- src/deploy/build_slug.py
+++ src/deploy/build_slug.py
@@ -21,6 +21,20 @@ flags.DEFINE_string('permuted_cloze_html_back_path', None,
                     'Permuted Cloze template HTML path')
 flags.DEFINE_string('permuted_cloze_css_path', None, 'Permuted Cloze CSS path')
 
+
+flags.DEFINE_string('keyboard_shortcut_uuid', None,
+                    'Keyboard Shortcut CrowdAnki UUID')
+flags.DEFINE_string('keyboard_shortcut_html_shortcut_to_effect_front_path', None,
+                    'Keyboard Shortcut Shortcut->Effect front template HTML path')
+flags.DEFINE_string('keyboard_shortcut_html_shortcut_to_effect_back_path', None,
+                    'Keyboard Shortcut Shortcut->Effect back template HTML path')
+flags.DEFINE_string('keyboard_shortcut_html_effect_to_shortcut_front_path', None,
+                    'Keyboard Shortcut Effect->Shortcut front template HTML path')
+flags.DEFINE_string('keyboard_shortcut_html_effect_to_shortcut_back_path', None,
+                    'Keyboard Shortcut Effect->Shortcut back template HTML path')
+flags.DEFINE_string('keyboard_shortcut_css_path',
+                    None, 'Permuted Cloze CSS path')
+
 flags.DEFINE_string('output_slug_path', None, 'Output slug path')
 FLAGS = flags.FLAGS
 
@@ -51,6 +65,29 @@ def main(_):
             'Cloze': {
                 'qfmt': read_file(FLAGS.permuted_cloze_html_front_path),
                 'qfmt': read_file(FLAGS.permuted_cloze_html_back_path),
+            }
+        }
+    }
+    if FLAGS.keyboard_shortcut_uuid in slug:
+        raise Exception("Duplicated UUID")
+    slug[FLAGS.keyboard_shortcut_uuid] = {
+        'css': read_file(FLAGS.keyboard_shortcut_css_path),
+        'templates': {
+            'Effect to Shortcut': {
+                'qfmt':
+                read_file(
+                    FLAGS.keyboard_shortcut_html_effect_to_shortcut_front_path),
+                'qfmt':
+                read_file(
+                    FLAGS.keyboard_shortcut_html_effect_to_shortcut_back_path),
+            },
+            'Shortcut to Effect': {
+                'qfmt':
+                read_file(
+                    FLAGS.keyboard_shortcut_html_shortcut_to_effect_front_path),
+                'qfmt':
+                read_file(
+                    FLAGS.keyboard_shortcut_html_shortcut_to_effect_back_path),
             }
         }
     }
