78b39e938784392d2f04c10e31ad19017c69104a
Michal Pokorný (Rai) <agentydragon@gmail.com>
2019-12-08 18:25:33 +0100

    Fucking Bazel dark magic right there

---
+++ src/anki_template.bzl
@@ -0,0 +1,102 @@
+AnkiTemplateInfo = provider(fields = [
+    "human_name",
+    "question_html",
+    "answer_html",
+])
+
+def _anki_template_impl(ctx):
+    print("Anki template {} question={} answer={} human name={}".format(ctx.label, ctx.attr.question_html, ctx.attr.answer_html, ctx.attr.human_name))
+    return [AnkiTemplateInfo(human_name = ctx.attr.human_name,
+                             question_html = ctx.attr.question_html,
+                             answer_html = ctx.attr.answer_html)]
+
+anki_template = rule(
+    attrs = {
+        "human_name": attr.string(mandatory = True),
+        "question_html": attr.label(
+            mandatory = True,
+            allow_files = True,
+        ),
+        "answer_html": attr.label(
+            mandatory = True,
+            allow_files = True,
+        ),
+    },
+    implementation = _anki_template_impl,
+)
+
+AnkiModelInfo = provider(fields = [
+    "css",
+    "crowdanki_uuid",
+    "templates",
+])
+
+def _anki_model_impl(ctx):
+    print("Anki model label {} uuid {}".format(ctx.label, ctx.attr.crowdanki_uuid))
+    templates = []
+    for template in ctx.attr.templates:
+        template_info = template[AnkiTemplateInfo]
+        print("question={} answer={} human_name={}".format(template_info.question_html, template_info.answer_html, template_info.human_name))
+        templates.append(template_info)
+
+    return [AnkiModelInfo(crowdanki_uuid = ctx.attr.crowdanki_uuid, templates=templates, css=ctx.attr.css)]
+
+anki_model = rule(
+    attrs = {
+        "crowdanki_uuid": attr.string(mandatory = True),
+        "templates": attr.label_list(
+            mandatory = True,
+            allow_empty = False,
+        ),
+        "css": attr.label(
+            mandatory = True,
+            allow_files = True,
+        ),
+    },
+    implementation = _anki_model_impl,
+)
+
+def _anki_slug_impl(ctx):
+    print("making slug")
+    all_files = []
+    args = ctx.actions.args()
+    models = []
+    for model in ctx.attr.models:
+        info = model[AnkiModelInfo]
+        all_files.extend(info.css.files.to_list())
+        templates = []
+        for template in info.templates:
+            q_html = template.question_html.files.to_list()
+            a_html = template.answer_html.files.to_list()
+            all_files.extend(q_html)
+            all_files.extend(a_html)
+            templates.append(struct(question_html=q_html[0].path,answer_html=a_html[0].path,human_name=template.human_name))
+        models.append(struct(crowdanki_uuid=info.crowdanki_uuid, templates=templates, css=info.css.files.to_list()[0].path))
+    inp = struct(models=models)
+    args.add("--models", inp.to_json())
+    args.add("--output_file", ctx.outputs.output_json)
+    args.add("--alsologtostderr")
+
+    print('all files: {}'.format(all_files))
+
+    ctx.actions.run(
+        inputs = all_files,
+        outputs = [ctx.outputs.output_json],
+        executable=ctx.executable.build_slug,
+        arguments=[args]
+    )
+
+anki_slug = rule(
+    attrs = {
+        "models": attr.label_list(mandatory = True),
+        "output_json": attr.output(mandatory = True),
+        "build_slug": attr.label(
+            executable = True,
+            cfg = "host",
+            allow_files = True,
+            default =
+                Label("//src/deploy:build_slug"),
+        ),
+    },
+    implementation = _anki_slug_impl,
+)
--- src/deploy/build_slug.py
+++ src/deploy/build_slug.py
@@ -8,34 +8,37 @@ from absl import flags
 
 import json
 
-flags.DEFINE_string('cloze_uuid', None, 'Cloze CrowdAnki UUID')
-flags.DEFINE_string('cloze_html_front_path', None, 'Cloze template HTML path')
-flags.DEFINE_string('cloze_html_back_path', None, 'Cloze template HTML path')
-flags.DEFINE_string('cloze_css_path', None, 'Cloze CSS path')
+flags.DEFINE_string('models', None, 'input files')
+flags.DEFINE_string('output_file', None, 'output file')
 
-flags.DEFINE_string('permuted_cloze_uuid', None,
-                    'Permuted Cloze CrowdAnki UUID')
-flags.DEFINE_string('permuted_cloze_html_front_path', None,
-                    'Permuted Cloze template HTML path')
-flags.DEFINE_string('permuted_cloze_html_back_path', None,
-                    'Permuted Cloze template HTML path')
-flags.DEFINE_string('permuted_cloze_css_path', None, 'Permuted Cloze CSS path')
+# flags.DEFINE_string('cloze_uuid', None, 'Cloze CrowdAnki UUID')
+# flags.DEFINE_string('cloze_html_front_path', None, 'Cloze template HTML path')
+# flags.DEFINE_string('cloze_html_back_path', None, 'Cloze template HTML path')
+# flags.DEFINE_string('cloze_css_path', None, 'Cloze CSS path')
+#
+# flags.DEFINE_string('permuted_cloze_uuid', None,
+#                     'Permuted Cloze CrowdAnki UUID')
+# flags.DEFINE_string('permuted_cloze_html_front_path', None,
+#                     'Permuted Cloze template HTML path')
+# flags.DEFINE_string('permuted_cloze_html_back_path', None,
+#                     'Permuted Cloze template HTML path')
+# flags.DEFINE_string('permuted_cloze_css_path', None, 'Permuted Cloze CSS path')
+#
+#
+# flags.DEFINE_string('keyboard_shortcut_uuid', None,
+#                     'Keyboard Shortcut CrowdAnki UUID')
+# flags.DEFINE_string('keyboard_shortcut_html_shortcut_to_effect_front_path', None,
+#                     'Keyboard Shortcut Shortcut->Effect front template HTML path')
+# flags.DEFINE_string('keyboard_shortcut_html_shortcut_to_effect_back_path', None,
+#                     'Keyboard Shortcut Shortcut->Effect back template HTML path')
+# flags.DEFINE_string('keyboard_shortcut_html_effect_to_shortcut_front_path', None,
+#                     'Keyboard Shortcut Effect->Shortcut front template HTML path')
+# flags.DEFINE_string('keyboard_shortcut_html_effect_to_shortcut_back_path', None,
+#                     'Keyboard Shortcut Effect->Shortcut back template HTML path')
+# flags.DEFINE_string('keyboard_shortcut_css_path',
+#                     None, 'Permuted Cloze CSS path')
 
-
-flags.DEFINE_string('keyboard_shortcut_uuid', None,
-                    'Keyboard Shortcut CrowdAnki UUID')
-flags.DEFINE_string('keyboard_shortcut_html_shortcut_to_effect_front_path', None,
-                    'Keyboard Shortcut Shortcut->Effect front template HTML path')
-flags.DEFINE_string('keyboard_shortcut_html_shortcut_to_effect_back_path', None,
-                    'Keyboard Shortcut Shortcut->Effect back template HTML path')
-flags.DEFINE_string('keyboard_shortcut_html_effect_to_shortcut_front_path', None,
-                    'Keyboard Shortcut Effect->Shortcut front template HTML path')
-flags.DEFINE_string('keyboard_shortcut_html_effect_to_shortcut_back_path', None,
-                    'Keyboard Shortcut Effect->Shortcut back template HTML path')
-flags.DEFINE_string('keyboard_shortcut_css_path',
-                    None, 'Permuted Cloze CSS path')
-
-flags.DEFINE_string('output_slug_path', None, 'Output slug path')
+#flags.DEFINE_string('output_slug_path', None, 'Output slug path')
 FLAGS = flags.FLAGS
 
 
@@ -46,65 +49,79 @@ def read_file(path):
 
 def main(_):
     slug = {}
-    if FLAGS.cloze_uuid in slug:
-        raise Exception("Duplicated UUID")
-    slug[FLAGS.cloze_uuid] = {
-        'css': read_file(FLAGS.cloze_css_path),
-        'templates': {
-            'Cloze': {
-                'qfmt': read_file(FLAGS.cloze_html_front_path),
-                'qfmt': read_file(FLAGS.cloze_html_back_path),
+    for model in json.loads(FLAGS.models)['models']:
+        logging.info("%s", model)
+        uuid = model['crowdanki_uuid']
+        if uuid in slug:
+            raise Exception("duplicated UUID: " + uuid)
+        templates = {}
+        for template in model['templates']:
+            human_name = template['human_name']
+            if human_name in templates:
+                raise Exception("duplicated human_name " +
+                                human_name + " in UUID " + uuid)
+            templates[human_name] = {
+                'qfmt': read_file(template['question_html']),
+                'qfmt': read_file(template['answer_html']),
             }
+        slug[uuid] = {
+            'css': read_file(model['css']),
+            'templates': templates
         }
-    }
-    if FLAGS.permuted_cloze_uuid in slug:
-        raise Exception("Duplicated UUID")
-    slug[FLAGS.permuted_cloze_uuid] = {
-        'css': read_file(FLAGS.permuted_cloze_css_path),
-        'templates': {
-            'Cloze': {
-                'qfmt': read_file(FLAGS.permuted_cloze_html_front_path),
-                'qfmt': read_file(FLAGS.permuted_cloze_html_back_path),
-            }
-        }
-    }
-    if FLAGS.keyboard_shortcut_uuid in slug:
-        raise Exception("Duplicated UUID")
-    slug[FLAGS.keyboard_shortcut_uuid] = {
-        'css': read_file(FLAGS.keyboard_shortcut_css_path),
-        'templates': {
-            'Effect to Shortcut': {
-                'qfmt':
-                read_file(
-                    FLAGS.keyboard_shortcut_html_effect_to_shortcut_front_path),
-                'qfmt':
-                read_file(
-                    FLAGS.keyboard_shortcut_html_effect_to_shortcut_back_path),
-            },
-            'Shortcut to Effect': {
-                'qfmt':
-                read_file(
-                    FLAGS.keyboard_shortcut_html_shortcut_to_effect_front_path),
-                'qfmt':
-                read_file(
-                    FLAGS.keyboard_shortcut_html_shortcut_to_effect_back_path),
-            }
-        }
-    }
-    with open(FLAGS.output_slug_path, 'w') as f:
+    with open(FLAGS.output_file, 'w') as f:
         json.dump(slug, f)
+
+#    slug = {}
+#    if FLAGS.cloze_uuid in slug:
+#        raise Exception("Duplicated UUID")
+#    slug[FLAGS.cloze_uuid] = {
+#        'css': read_file(FLAGS.cloze_css_path),
+#        'templates': {
+#            'Cloze': {
+#                'qfmt': read_file(FLAGS.cloze_html_front_path),
+#                'qfmt': read_file(FLAGS.cloze_html_back_path),
+#            }
+#        }
+#    }
+#    if FLAGS.keyboard_shortcut_uuid in slug:
+#        raise Exception("Duplicated UUID")
+#    slug[FLAGS.keyboard_shortcut_uuid] = {
+#        'css': read_file(FLAGS.keyboard_shortcut_css_path),
+#        'templates': {
+#            'Effect to Shortcut': {
+#                'qfmt':
+#                read_file(
+#                    FLAGS.keyboard_shortcut_html_effect_to_shortcut_front_path),
+#                'qfmt':
+#                read_file(
+#                    FLAGS.keyboard_shortcut_html_effect_to_shortcut_back_path),
+#            },
+#            '': {
+#                'qfmt':
+#                read_file(
+#                    FLAGS.keyboard_shortcut_html_shortcut_to_effect_front_path),
+#                'qfmt':
+#                read_file(
+#                    FLAGS.keyboard_shortcut_html_shortcut_to_effect_back_path),
+#            }
+#        }
+#    }
+#    with open(FLAGS.output_slug_path, 'w') as f:
+#        json.dump(slug, f)
 
 
 if __name__ == '__main__':
-    flags.mark_flag_as_required('cloze_uuid')
-    flags.mark_flag_as_required('cloze_html_front_path')
-    flags.mark_flag_as_required('cloze_html_back_path')
-    flags.mark_flag_as_required('cloze_css_path')
+    flags.mark_flag_as_required('output_file')
+    flags.mark_flag_as_required('models')
+ #   flags.mark_flag_as_required('cloze_uuid')
+ #   flags.mark_flag_as_required('cloze_html_front_path')
+ #   flags.mark_flag_as_required('cloze_html_back_path')
+ #   flags.mark_flag_as_required('cloze_css_path')
 
-    flags.mark_flag_as_required('permuted_cloze_uuid')
-    flags.mark_flag_as_required('permuted_cloze_html_front_path')
-    flags.mark_flag_as_required('permuted_cloze_html_back_path')
-    flags.mark_flag_as_required('permuted_cloze_css_path')
+ #   flags.mark_flag_as_required('permuted_cloze_uuid')
+ #   flags.mark_flag_as_required('permuted_cloze_html_front_path')
+ #   flags.mark_flag_as_required('permuted_cloze_html_back_path')
+ #   flags.mark_flag_as_required('permuted_cloze_css_path')
 
-    flags.mark_flag_as_required('output_slug_path')
+ #   flags.mark_flag_as_required('output_slug_path')
     app.run(main)
