27be120b940661c10ee9074f75029ce23600e4be
Michal Pokorný (Rai) <agentydragon@gmail.com>
2019-12-08 18:55:33 +0100

    Split models by managed/unmanaged

--- src/deploy/list_models.py
+++ src/deploy/list_models.py
@@ -6,6 +6,7 @@ bazel run //src/deploy:list_models -- \
 from absl import app
 from absl import logging
 from absl import flags
+from rules_python.python.runfiles import runfiles
 
 import operator
 import os
@@ -16,15 +17,56 @@ sys.path.append('/usr/share/anki')
 import anki
 
 flags.DEFINE_string('collection_path', None, 'Path to .anki2 collection')
+flags.DEFINE_string('slug_path', None, 'Path to update JSON slug')
 FLAGS = flags.FLAGS
 
 
 def main(_):
+    slug_path = FLAGS.slug_path
+    if slug_path is None:
+        path = runfiles.Create().Rlocation("__main__/src/deploy/slug.json")
+        logging.info("Loading slug from runfiles: %s", path)
+        slug_path = path
+
+    with open(slug_path, 'r') as f:
+        slug = json.load(f)
+    managed_uuids = slug.keys()
+    logging.info("Managed UUIDs:")
+    for key in managed_uuids:
+        logging.info("- %s", key)
+
     collection = anki.Collection(FLAGS.collection_path)
     found = None
+    seen_uuids = set()
+    managed_models = []
+    untracked_models = []
     for model in collection.models.all():
-        logging.info("%s (%s):", model['name'], model['crowdanki_uuid'])
-        logging.info("%s", [tmpl['name'] for tmpl in model['tmpls']])
+        crowdanki_uuid = model['crowdanki_uuid']
+        is_managed = (crowdanki_uuid in managed_uuids)
+        if is_managed:
+            managed_models.append(model)
+        else:
+            untracked_models.append(model)
+        seen_uuids.add(crowdanki_uuid)
+
+    logging.info("")
+    logging.info("Managed models:")
+    for model in managed_models:
+        logging.info("- %s (%s):", model['name'], crowdanki_uuid)
+        for tmpl in model['tmpls']:
+            logging.info("    %s", tmpl['name'])
+
+    logging.info("")
+    logging.info("Untracked models:")
+    for model in untracked_models:
+        logging.info("- %s (%s):", model['name'], crowdanki_uuid)
+        for tmpl in model['tmpls']:
+            logging.info("    %s", tmpl['name'])
+
+    missing_managed = set(managed_uuids) - seen_uuids
+    if len(missing_managed) > 0:
+        logging.error('UUIDs managed but not in collection: %s',
+                      missing_managed)
 
 
 if __name__ == '__main__':
