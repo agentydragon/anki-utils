909c2aa48cf1970e4e6f4795a79eb7176ee114d8
Michal Pokorný (Rai) <agentydragon@gmail.com>
2019-12-08 18:45:39 +0100

    deploy from slug

--- src/deploy/deploy.py
+++ src/deploy/deploy.py
@@ -1,6 +1,14 @@
+"""
+blaze run //src/deploy -- --alsologtostderr --collection_path=<...>
+
+If all looks fine, run also --dry_run=False.
+"""
+
 from absl import app
 from absl import logging
 from absl import flags
+
+from rules_python.python.runfiles import runfiles
 
 import os
 import json
@@ -10,7 +18,8 @@ sys.path.append('/usr/share/anki')
 import anki
 
 flags.DEFINE_string('collection_path', None, 'Path to .anki2 collection')
-flags.DEFINE_string('update_slug_path', None, 'Path to update JSON slug')
+flags.DEFINE_string('slug_path', None, 'Path to update JSON slug')
+flags.DEFINE_bool('dry_run', True, 'Whether to actually do it')
 FLAGS = flags.FLAGS
 
 # Example update slug:
@@ -44,39 +53,53 @@ def find_model_by_uuid(collection, uuid):
     raise KeyError("Model with UUID " + uuid + " not found.")
 
 
+def apply_model_update(model, model_update):
+    if 'css' in model_update:
+        model['css'] = model_update['css']
+
+    for template_name, template_content in model_update['templates'].items():
+        template_found = None
+        for template in model['tmpls']:
+            if template['name'] == template_name:
+                if template_found:
+                    raise Exception(
+                        'Multiple templates with name {}'.format(template_name))
+                template_found = template
+        if not template_found:
+            raise KeyError(
+                'Template with name {} not found'.format(template_name))
+
+        if 'qfmt' in template_content:
+            template_found['qfmt'] = template_content['qfmt']
+        if 'afmt' in template_content:
+            template_found['afmt'] = template_content['afmt']
+
+
+def apply_slug(collection, slug):
+    for uuid, model_update in slug.items():
+        logging.info("Updating model with UUID %s", uuid)
+        model = find_model_by_uuid(collection, uuid)
+        apply_model_update(model, model_update)
+        if not FLAGS.dry_run:
+            collection.models.update(model)
+
+
 def main(_):
     collection = anki.Collection(FLAGS.collection_path)
 
-    with open(FLAGS.update_slug_path, 'r') as f:
-        update_slug = json.load(f)
+    slug_path = FLAGS.slug_path
+    if slug_path is None:
+        path = runfiles.Create().Rlocation("__main__/src/deploy/slug.json")
+        logging.info("Loading slug from runfiles: %s", path)
+        slug_path = path
 
-    for uuid, model_update in update_slug.iteritems():
-        logging.info("Updating model with UUID %s", uuid)
-        model = find_model_by_uuid(uuid)
+    with open(slug_path, 'r') as f:
+        slug = json.load(f)
 
-        if 'css' in model_update:
-            model['css'] = model_update['css']
+    apply_slug(collection, slug)
 
-        for template_name, template_content in model_update:
-            template_found = None
-            for template in model['tmpls']:
-                if template['name'] == template_name:
-                    if template_found:
-                        raise Exception(
-                            "Multiple templates with name " + template_name)
-                    template_found = template
-            if not template_found:
-                raise KeyError("Template with name " +
-                               template_name + " not found")
-
-            if 'qfmt' in template_content:
-                template_found['qfmt'] = template_content['qfmt']
-            if 'afmt' in template_content:
-                template_found['afmt'] = template_content['afmt']
-
-        collection.models.update(model)
-
-    collection.models.flush()
+    if not FLAGS.dry_run:
+        collection.models.flush()
 
 
 if __name__ == '__main__':
