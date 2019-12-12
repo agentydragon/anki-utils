e6acdf3e2a19bfc66e487c31be3fa170ac94a99e
Michal Pokorný (Rai) <agentydragon@gmail.com>
2019-12-12 23:26:53 +0100

    Add headings from tags

--- src/shared_styles/heading.js
+++ src/shared_styles/heading.js
@@ -20,18 +20,64 @@ Rai.obtainNoteFields = function() {
   Rai.NOTE_FIELDS = {
     HEADING : Rai.obtainFieldFromId("agentydragon-heading"),
     DECK : Rai.obtainFieldFromId("agentydragon-deck"),
+    TAGS : Rai.obtainFieldFromId("agentydragon-tags"),
   };
+};
+
+Rai.tagIsStrictlyUnderTag = function(
+    tag, parentTag) { return tag.startsWith(parentTag + '::'); };
+
+Rai.tagIsUnderTag = function(tag, parentTag) {
+  return tag == parentTag || Rai.tagIsStrictlyUnderTag(tag, parentTag);
+};
+
+Rai.headingFromTag = function(tag) {
+  const parts = tag.split('::');
+  return parts[parts.length - 1]
+      .split('-')
+      .map(word => word.substring(0, 1).toUpperCase() + word.substring(1))
+      .join(' ');
+};
+
+// TODO: this should result in 'german':
+// languages languages::language::german
+// languages::language::german::4k_words_by_frequency
+// languages::language::german::4k_words_by_frequency::Level::166 leech
+
+Rai.headingFromTags = function() {
+  const tags = Rai.NOTE_FIELDS.TAGS;
+  const individualTags =
+      tags.split(' ').filter(tag => !Rai.tagIsUnderTag(tag, "todo"));
+  // Remove non-leaf tags.
+  const tagIsNonleaf = tag => individualTags.some(
+      candidateChild => Rai.tagIsStrictlyUnderTag(candidateChild, tag));
+  const tagIsLeaf = tag => !tagIsNonleaf(tag);
+  const leafTags = individualTags.filter(tagIsLeaf);
+  const candidateHeadings = leafTags.map(Rai.headingFromTag).sort();
+  if (candidateHeadings.length == 0) {
+    return null;
+  }
+  return candidateHeadings[0];
 };
 
 Rai.obtainHeading = function() {
   const headerInContent = document.querySelector("#agentydragon-content h1");
   if (headerInContent) {
+    if (Rai.NOTE_FIELDS.HEADING && Rai.NOTE_FIELDS.HEADING.length > 0) {
+      Rai.doLog("warning", "Both Heading field and inline heading specified.");
+    }
     headerInContent.remove();
     return headerInContent.textContent;
   }
-  if (Rai.NOTE_FIELDS.HEADING) {
+  // Use the heading specified in the note, if given.
+  if (Rai.NOTE_FIELDS.HEADING && Rai.NOTE_FIELDS.HEADING.length > 0) {
     return Rai.NOTE_FIELDS.HEADING;
   }
+  const headingFromTags = Rai.headingFromTags();
+  if (headingFromTags) {
+    return headingFromTags;
+  }
+  // Last resort: show the deck name
   return getLastDeckComponent(Rai.NOTE_FIELDS.DECK);
 };
 
