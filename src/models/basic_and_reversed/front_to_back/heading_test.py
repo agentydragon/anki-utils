21b91f4955b0672530f75a3150ffe2fc3b5b098b
Michal Pokorný (Rai) <agentydragon@gmail.com>
2019-12-15 13:41:04 +0100

    Move shared stuff into common testcase base

--- src/models/basic_and_reversed/front_to_back/heading_test.py
+++ src/models/basic_and_reversed/front_to_back/heading_test.py
@@ -1,58 +1,12 @@
-import time
-import unittest
 from absl.testing import absltest
 from absl.testing import parameterized
-from testing.web import webtest
-from selenium.webdriver.support.ui import WebDriverWait
-from rules_python.python.runfiles import runfiles
-
-import sys
-sys.path.append('/usr/share/anki')
-from anki import template
 
 FRONT_RUNFILES_PATH = "anki_utils/src/models/basic_and_reversed/front_to_back/front.expanded.html"
 
+from src.testing import card_testcase
 
-class HeadingTest(parameterized.TestCase):
-  def setUp(self):
-    self.driver = webtest.new_webdriver_session()
-    self.runfiles = runfiles.Create()
 
-  def tearDown(self):
-    try:
-      self.driver.quit()
-    finally:
-      self.driver = None
-
-  def load_front_html(self):
-    with open(self.runfiles.Rlocation(FRONT_RUNFILES_PATH)) as f:
-        return f.read()
-
-  def wait_until_loaded(self):
-    def check_done(driver):
-      state = driver.execute_script('return document.readyState')
-      return state == 'complete'
-
-    WebDriverWait(self.driver, 10).until(check_done)
-
-  def open_html(self, html):
-    html = '<div id="card">' + html + '</div>'
-    filename = '/tmp/x.html'
-    with open(filename, 'w') as f:
-      f.write(html)
-    self.driver.get("file://{}".format(filename))
-
-  def get_log(self):
-    return self.driver.find_element_by_id('agentydragon-log').text
-
-  def load_front_card(self, fields):
-    html = template.render(template=self.load_front_html(), context=fields)
-    self.open_html(html)
-    self.wait_until_loaded()
-
-  def get_heading(self):
-    return self.driver.find_element_by_tag_name('h1').text
-
+class HeadingTest(card_testcase.CardTestCase, parameterized.TestCase):
   @parameterized.parameters(
       ({'Heading': 'Greeting'}, 'Greeting'),
       ({'Tags': 'geography'}, 'Geography'),
@@ -61,7 +15,8 @@ class HeadingTest(parameterized.TestCase):
       ({'Tags': 'javascript'}, 'JavaScript'),
   )
   def test_heading(self, fields, expected_heading):
-    self.load_front_card(fields)
+    self.open_card_from_runfiles(runfiles_path=FRONT_RUNFILES_PATH,
+                                 fields=fields)
     self.assertEqual(self.get_heading(), expected_heading)
 
 
---
+++ src/testing/card_testcase.py
@@ -0,0 +1,56 @@
+from absl.testing import absltest
+from testing.web import webtest
+from selenium.webdriver.support.ui import WebDriverWait
+from rules_python.python.runfiles import runfiles
+
+import sys
+sys.path.append('/usr/share/anki')
+from anki import template as anki_template
+
+from src.testing import card_testcase
+
+
+class CardTestCase(absltest.TestCase):
+  def setUp(self):
+    self.driver = webtest.new_webdriver_session()
+    self.runfiles = runfiles.Create()
+
+  def tearDown(self):
+    try:
+      self.driver.quit()
+    finally:
+      self.driver = None
+
+  def wait_until_loaded(self):
+    def check_done(driver):
+      state = driver.execute_script('return document.readyState')
+      return state == 'complete'
+
+    WebDriverWait(self.driver, 10).until(check_done)
+
+  def load_from_runfiles(self, runfiles_path):
+    with open(self.runfiles.Rlocation(runfiles_path)) as f:
+        return f.read()
+
+  def open_card_from_runfiles(self, runfiles_path, fields):
+    template = self.load_from_runfiles(runfiles_path)
+    html = anki_template.render(template=template, context=fields)
+    self.open_html(html)
+    self.wait_until_loaded()
+
+  def open_html(self, html):
+    html = '<div id="card">' + html + '</div>'
+    filename = '/tmp/x.html'
+    with open(filename, 'w') as f:
+      f.write(html)
+    self.driver.get("file://{}".format(filename))
+
+  def get_log(self):
+    return self.driver.find_element_by_id('agentydragon-log').text
+
+  def get_heading(self):
+    return self.driver.find_element_by_tag_name('h1').text
+
+
+if __name__ == "__main__":
+  absltest.main()
