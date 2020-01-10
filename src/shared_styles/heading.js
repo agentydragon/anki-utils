e5a6286d0785b12639f43d866c4c4ccd20055ef9
Michal Pokorný (Rai) <agentydragon@gmail.com>
2020-01-10 02:00:20 +0100

    more

--- src/shared_styles/heading.js
+++ src/shared_styles/heading.js
@@ -31,6 +31,7 @@ const SPECIAL_TITLECASE = new Map([
   [ 'cpp::core', 'C++ core' ],
   [ 'cpp::stl', 'C++ STL' ],
   [ 'cs::languages', 'Programming languages' ],
+  [ 'css', 'CSS' ],
   [ 'css::selector::attribute', 'CSS attribute selectors' ],
   [ 'css::selector::pseudoclass', 'CSS pseudoclasses' ],
   [ 'czech-cases-latin', 'Latin names of Czech cases' ],
